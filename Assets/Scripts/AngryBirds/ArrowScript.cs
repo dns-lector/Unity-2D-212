af697930977ff89df82959cbeaae784677b584aa
dns-lector <157026463+dns-lector@users.noreply.github.com>
2024-11-05 17:50:17 +0200

    Bird

---
+++ Assets/Scripts/AngryBirds/ArrowScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class ArrowScript : MonoBehaviour
+{
+    [SerializeField]
+    private Transform rotAnchor;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float dy = Input.GetAxis("Vertical");   // "сигнал" з усіх
+        // пристроїв, що відповідають за управління по вертикалі
+        float currentAngle = this.transform.eulerAngles.z;
+        if(currentAngle > 180)
+        {
+            currentAngle -= 360;
+        }
+        if (-45 < currentAngle + dy && currentAngle + dy < 45)
+        {
+            this.transform.RotateAround(rotAnchor.position, Vector3.forward, dy);
+        }
+    }
+}
+/* Скрипт управління "Стрілкою"
+ * за натиском "Вгору"/"Вниз" (W/S, джойстик) стрілка змінює кут нахилу
+ * 
+ * Д.З. Створити нову сцену
+ * Зробити її аналогічною попередній сцені, тільки розмістити іншого персонажа
+ * Змінити стартову позицію
+ * Для обох сцен підібрати максимальний та мінімальний кут нахилу Стрілки
+ */
---
+++ Assets/Scripts/AngryBirds/BirdScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class BirdScript : MonoBehaviour
+{
+    [SerializeField]
+    private Transform arrow;
+    private Rigidbody2D rb2d;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb2d.AddForce(1000 * arrow.right);
+        }
+    }
+}
+/* Управління персонажем (птахом)
+ * За натисненням кнопки "Пробіл" одноразово прикладаємо силу
+ * у напрямі Стрілки
+ */
